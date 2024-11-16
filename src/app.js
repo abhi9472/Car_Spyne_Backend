eb3837318918fbddc2fab87a144760d18208b4bf
Abhishek <abhishekkumarcse2015@gmail.com>
2024-11-17 00:55:49 +0530

    Minor changes

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
+// app.use(cors());
 
 app.use(
   cors({
@@ -10,7 +11,7 @@ app.use(
     credentials: true,
   }),
 );
-app.use(cors({ origin: "https://spynecar.vercel.app" }));
+// app.use(cors({ origin: "https://spynecar.vercel.app" }));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
