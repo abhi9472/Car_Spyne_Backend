3e7336787b22f36ad63ea8c9c34e40888835098c
Abhishek <abhishekkumarcse2015@gmail.com>
2024-11-17 00:30:31 +0530

    Cors issue Resolved

--- src/app.js
+++ src/app.js
@@ -1,24 +1,25 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: true,
-    credentials: true
-}))
+    credentials: true,
+  }),
+);
+app.use(cors({ origin: "https://spynecar.vercel.app" }));
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
-
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // const app=express()
-import userRouter from './routes/user.routes.js'
+import userRouter from "./routes/user.routes.js";
 // import { registerUser } from "./controllers/user.controller.js"
 
-app.use("/api/v1/users",userRouter)
-export {app}
-
+app.use("/api/v1/users", userRouter);
+export { app };
